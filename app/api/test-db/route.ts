c921ffd9ca92e1a3c3b288a35968ceb24a1cc877
Hasibullah-cyber <hasdibsolder506@gmail.com>
2025-07-10 23:33:52 +0600

    Update route.ts

--- app/api/test-db/route.ts
+++ app/api/test-db/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { testDatabaseConnection } from '@/lib/db' // ✅ update this if your db file is elsewhere
+import { testDatabaseConnection } from '@/lib/database.ts' // ✅ update this if your db file is elsewhere
 
 export async function GET() {
   const success = await testDatabaseConnection()
