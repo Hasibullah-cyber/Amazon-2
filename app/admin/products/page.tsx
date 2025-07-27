82b145889d7b78fa287fed178c562cc98f3d6082
Hasibullah-cyber <hasdibsolder506@gmail.com>
2025-07-28 04:16:57 +0600

    Update page.tsx

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,5 +1,4 @@
 // app/admin/products/page.tsx
-
 "use client"
 import { useEffect, useState, useCallback, useMemo } from "react"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card"
